a070c4458a693f88f4fadd18ab66e45bc65a8ae8
万斌 <a13308263216@gmail.com>
2024-05-16 16:46:19 +0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
-/** @type {import('next').NextConfig} */
-const withOffline = require('next-offline')
+const withPWA = require('next-pwa')({
+  // disable: process.env.NODE_ENV === 'development', // 测试环境禁用
+  sw: 'sw.js',                    // service workder注册入口文件，可选，默认为sw.js
+  dest: 'public',                 // sw.js文件输出目录
+  runtimeCaching : [{
+    urlPattern: /.*/,                 // 运行时缓存路由
+    handler: 'StaleWhileRevalidate'   // 运行时缓存策略
+  }],
+})
 
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^http?.*/,
-        handler: 'NetworkFirst',
-        options: { cacheName: 'offlineCache', expiration: { maxEntries: 200 } },
-      },
-    ],
-  },
-});
+module.exports = withPWA({
+  // next.js config
+})
